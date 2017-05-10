4091c6331a79ae6ef91ca78be4b9abae86f642bd
Glen Maddern <glenmaddern@gmail.com>
2017-05-10 13:07:47 +0200

    I like this api

--- src/superposition/index.js
+++ src/superposition/index.js
@@ -1,39 +1,47 @@
-export default componentsRef => {
-  const components = Object.assign({}, componentsRef)
-  let collapsed = false
-
-  const wavefunction = {}
-  Object.keys(components).forEach(name => {
-    Object.defineProperty(wavefunction, name, {
-      enumerable: true,
-      get() {
-        const virtual = components[name](wavefunction)
-        if (typeof virtual === "function") {
-          return (...args) => {
-            if (!collapsed) collapsed = name
-            return components[name](wavefunction)(...args)
-          }
-        } else if (typeof virtual === "object") {
-          return Object.create(virtual, Object.keys(virtual).reduce((defs, k) => {
-            return {...defs, [k]: {
-              enumerable: true,
-              get() {
-                if (!collapsed) collapsed = name
-                return components[name](wavefunction)[k]
-              }
-            }}
-          }, {}))
-        } else {
-          return virtual
-        }
-      }
-    })
-  })
-
-  wavefunction.change = comps => {
-    if (collapsed) throw new Error(`Collapsed due to '${collapsed}' having been called.`)
-    return Object.assign(components, comps)
+export default class Superposition {
+  constructor(initialState) {
+    this.initialState = initialState
   }
 
-  return wavefunction
+  createWaveFunction() {
+    const components = Object.assign({}, this.initialState)
+    let collapsed = false
+
+    const wavefunction = {}
+    Object.keys(components).forEach(name => {
+      Object.defineProperty(wavefunction, name, {
+        enumerable: true,
+        get() {
+          const virtual = components[name](wavefunction)
+          if (typeof virtual === 'function') {
+            return (...args) => {
+              if (!collapsed) collapsed = name
+              return components[name](wavefunction)(...args)
+            }
+          } else if (typeof virtual === 'object') {
+            return Object.create(virtual, Object.keys(virtual).reduce((defs, k) => ({
+              ...defs,
+              [k]: {
+                enumerable: true,
+                get() {
+                  if (!collapsed) collapsed = name
+                  return components[name](wavefunction)[k]
+                },
+              },
+            }), {}))
+          } else {
+            return virtual
+          }
+        },
+      })
+    })
+
+    wavefunction.modify = comps => {
+      if (collapsed) throw new Error(`Collapsed due to '${collapsed}' having been called.`)
+      return Object.assign(components, comps)
+    }
+    wavefunction._superposition = this
+
+    return wavefunction
+  }
 }
--- src/superposition/index.test.js
+++ src/superposition/index.test.js
@@ -21,7 +21,7 @@ describe('basic', () => {
   })
 
   it('should be configurable before getting', () => {
-    wavefunction.change({ Constant: () => 5 })
+    wavefunction.modify({ Constant: () => 5 })
     const { Sum, Product } = wavefunction
     expect(Sum(9)).toBe(14)
     expect(Product(9)).toBe(45)
@@ -29,7 +29,7 @@ describe('basic', () => {
 
   it('should be configurable after getting', () => {
     const { Sum, Product } = wavefunction
-    wavefunction.change({ Constant: () => 5 })
+    wavefunction.modify({ Constant: () => 5 })
     expect(Sum(9)).toBe(14)
     expect(Product(9)).toBe(45)
   })
@@ -37,13 +37,13 @@ describe('basic', () => {
   it('should not configurable after collapse', () => {
     const { Sum } = wavefunction
     expect(Sum(9)).toBe(16)
-    expect(() => wavefunction.change({ Constant: () => 5 })).toThrow("Collapsed due to 'Sum' having been called.")
+    expect(() => wavefunction.modify({ Constant: () => 5 })).toThrow("Collapsed due to 'Sum' having been called.")
   })
 
   it('should not configurable after collapse', () => {
     const { Product } = wavefunction
     expect(Product(9)).toBe(63)
-    expect(() => wavefunction.alter({ Constant: () => 5 })).toThrow("Collapsed due to 'Product' having been called.")
+    expect(() => wavefunction.modify({ Constant: () => 5 })).toThrow("Collapsed due to 'Product' having been called.")
   })
 })
 
@@ -52,39 +52,39 @@ describe('objects', () => {
   const Product = ({ Constants }) => () => Constants.x * Constants.y
   const Constants = () => ({ x: 5, y: 7 })
 
-  let system
+  let wavefunction
   beforeEach(() => {
-    system = new Superposition({
+    wavefunction = new Superposition({
       Sum,
       Product,
       Constants
-    })
+    }).createWaveFunction()
   })
 
   it('should run', () => {
-    const { Sum, Product } = system
+    const { Sum, Product } = wavefunction
     expect(Sum()).toBe(12)
     expect(Product()).toBe(35)
   })
 
   it('should be configurable before getting', () => {
-    system.alter({ Constants: () => ({ x: 6, y: 8 }) })
-    const { Sum, Product } = system
+    wavefunction.modify({ Constants: () => ({ x: 6, y: 8 }) })
+    const { Sum, Product } = wavefunction
     expect(Sum()).toBe(14)
     expect(Product()).toBe(48)
   })
 
   it('should be configurable after getting', () => {
-    const { Sum, Product } = system
-    system.alter({ Constants: () => ({ x: 6, y: 8 }) })
+    const { Sum, Product } = wavefunction
+    wavefunction.modify({ Constants: () => ({ x: 6, y: 8 }) })
     expect(Sum()).toBe(14)
     expect(Product()).toBe(48)
   })
 
   it('should collapse on property access', () => {
-    const { Constants } = system
+    const { Constants } = wavefunction
     expect(Constants.x).toBe(5)
-    expect(() => system.alter({ Constants: () => ({ x: 6, y: 8 }) })).toThrow("Collapsed due to 'Constants' having been called.")
+    expect(() => wavefunction.modify({ Constants: () => ({ x: 6, y: 8 }) })).toThrow("Collapsed due to 'Constants' having been called.")
   })
 })
 
@@ -96,35 +96,35 @@ describe('complex objects', () => {
   })
   const Constants = () => ({ x: 5, y: 7 })
 
-  let system
+  let wavefunction
   beforeEach(() => {
-    system = new Superposition({
+    wavefunction = new Superposition({
       Maths,
       Constants
-    })
+    }).createWaveFunction()
   })
 
   it('should run', () => {
-    const { Maths } = system
+    const { Maths } = wavefunction
     expect(Maths.sum()).toBe(12)
     expect(Maths.product()).toBe(35)
   })
 
   it('calling a function should collapse things', () => {
-    const { Maths } = system
+    const { Maths } = wavefunction
     expect(Maths.identity).toBe(1)
-    expect(() => system.alter()).toThrow("Collapsed due to 'Maths' having been called.")
+    expect(() => wavefunction.modify()).toThrow("Collapsed due to 'Maths' having been called.")
   })
 
   it('should allow Maths to be patched', () => {
-    const { Maths } = system
-    system.alter({Maths: ({ Constants }) => ({
+    const { Maths } = wavefunction
+    wavefunction.modify({Maths: ({ Constants }) => ({
       sum: () => Constants.x + Constants.x,
       product: () => Constants.y * Constants.y
     })})
     expect(Maths.sum()).toBe(10)
     expect(Maths.product()).toBe(49)
-    expect(() => system.alter()).toThrow("Collapsed due to 'Maths' having been called.")
+    expect(() => wavefunction.modify()).toThrow("Collapsed due to 'Maths' having been called.")
   })
 
 })
